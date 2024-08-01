a14a7a481515d5726744689c7cd3edc43b2587ca
ahmedalaadev <ahmedaboalaa012gmail.com>
2024-08-01 17:36:14 +0300

    template route

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,28 @@
+import { Route, Routes } from "react-router-dom";
 import "./globals.css";
+import SignInForm from "./_auth/forms/SignInForm";
+import SignUpForm from "./_auth/forms/SignUpForm";
+import AuthLayout from "./_auth/AuthLayout";
+import RootLayout from "./_root/RootLayout";
+import { Home } from "./_root/pages";
+
 
 function App() {
   return (
-    <>
-      <h1 className="text-3xl font-bold underline">Hello world!</h1>
-    </>
+    <main className="flex h-screen">
+      <Routes>
+        <Route element={<AuthLayout />}>
+          <Route path="/sign-in" element={<SignInForm />} />
+          <Route path="/sign-up" element={<SignUpForm />} />
+        </Route>
+
+        <Route element={<RootLayout />}>
+          <Route index element={<Home/>} />
+
+        </Route>
+
+      </Routes>
+    </main>
   );
 }
 
